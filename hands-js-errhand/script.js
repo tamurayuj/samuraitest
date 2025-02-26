976bc60b52fa06e7618c33bfe6cdbbcf61b58ec6
tamura <dorodoronuma@yahoo.co.jp>
2025-02-26 21:41:22 +0900

    微修正

--- hands-js-errhand/script.js
+++ hands-js-errhand/script.js
@@ -1,21 +1,20 @@
-function performDivision() {
-    const numerator = document.getElementById("numerator").value;
-    const denominator = document.getElementById("denominator").value;
-    const resultElement = document.getElementById("result");
+function exeute() {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const answer = document.getElementById("answer");
     
-
     try {
-        const result = numerator / denominator;
+        const result = a / b;
         // if (isNaN(result)) {
         //     throw new Error("無効な計算結果です");
         // }
         // if (!isFinite(result)) {
         //     throw new Error("０で割ってはいけません");
-        // }
-        resultElement.textContent = `結果: ${result}`;
+        // }        
+        answer.textContent = `結果: ${result}`;
     } catch (error) {
         console.error("エラーが発生しました:", error.message);
-        resultElement.textContent = `エラー: ${error.message}`;
+        answer.textContent = `エラー: ${error.message}`;
     } finally {
         // 最後に必ず実行される処理
         console.log("割り算が完了しました");
